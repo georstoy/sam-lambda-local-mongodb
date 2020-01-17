409b5aacefeb81750fae03bc0754d8a01e3fe813
georstoy <georstoy@gmail.com>
2020-01-17 12:30:56 +0200

    set sam invocation to use host network

--- sam-app/hello-world/app.ts
+++ sam-app/hello-world/app.ts
@@ -12,9 +12,12 @@ let response: ISuccessResponse;
 
 export const lambdaHandler = async (): Promise<ISuccessResponse | IErrorResponse> => {
     try {
-        // const url = "mongodb://potrebitel:parola@localhost:27017";
-        const url = "mongodb://host.docker.internal:27018";
+        /* Failed tries
+        const url = "mongodb://potrebitel:parola@database:27017";
+        */
+        const url = "mongodb://potrebitel:parola@localhost:27017";
         console.log("about to establish a connection to MongoDb...");
+        await new Promise((resolve) => setTimeout(resolve, 10000));
 
         const clientPromise = MongoClient.connect(url, { useUnifiedTopology: true });
 
