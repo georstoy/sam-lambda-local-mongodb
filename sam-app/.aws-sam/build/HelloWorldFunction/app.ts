f30bf89c0f2dcd18ad954fdb1e5b91c27724c28a
georstoy <georstoy@gmail.com>
2020-01-17 09:31:07 +0200

    WIP

--- sam-app/.aws-sam/build/HelloWorldFunction/app.ts
+++ sam-app/.aws-sam/build/HelloWorldFunction/app.ts
@@ -1,20 +1,17 @@
 import { MongoClient } from "mongodb";
 let response;
 
-interface IEvent {
-    name: string;
-}
-
-exports.lambdaHandler = async (event: IEvent/*, context*/) => {
+exports.lambdaHandler = async () => {
     try {
         const url = "mongodb://potrebitel:parola@localhost:27017";
+        console.log("about to establish a connection...");
         const client: MongoClient = await MongoClient.connect(url, { useUnifiedTopology: true });
-
+        console.log(client);
         response = {
             statusCode: 200,
             body: JSON.stringify({
                 message: "Successful connection",
-                // location: ret.data.trim()
+                client,
             }),
         };
     } catch (err) {
